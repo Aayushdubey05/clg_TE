96c0ce0c5de382407cbd4d55ad67efb9b9951900
Aayushdubey05 <dubeyaayush@gmail.com>
2024-12-15 17:16:40 +0530

    Added new changes to store the data from the dashboard to the database

--- Backend/src/controllers/dashboard.controller.js
+++ Backend/src/controllers/dashboard.controller.js
@@ -56,11 +56,48 @@ exports.uploadDocument = async (req, res) => {
             };
             await dashboard.update({ admission_documents: admissionDocs });
         } else if (document_type === 'twelft_marksheet'){
-            
+            const admissionDocs = dashboard.admission_documents;
+            admissionDocs.twelfth_marksheet = {
+                result: {
+                    score: score,
+                    uploaded_at: new Date(),
+                    verified: false,
+                    verified_by: null,
+                    version: (admissionDocs.twelfth_marksheet?.result?.version || 0)+1,
+                    remarks: null,
+                }
+            };
+            await dashboard.update({ admission_documents: admissionDocs });
+        } else {
+            if (year && semester) {
+                if(!dashboard.academic_records[`year_${year}`]){
+                    dashboard.academic_records[`year_${year}`] = {};
+                }
+                if(!dashboard.academic_records[`year_${year}`][`sem_${semester}`]){
+                    dashboard.academic_records[`year_${year}`][`sem_${semester}`] = {
+                        result: {
+                            score: null,  
+                            uploaded_at: null,
+                            verified: false,
+                            verified_by: null,
+                            version: 1,
+                            remarks: null
+                        }
+                    };
+                }
+                dashboard.academic_records[`year_${year}`][`sem_${semester}`].result.score = score;
+                await dashboard.update({ academic_records: dashboard.academic_records });
+            }
         }
+
+        res.status(200).json({message: "Data has been successfully uploaded"});
     }
     catch(error){
-
+        res.status(500).json({
+            message: "failed to upload the data",
+            error: error.message
+        });
     }
 };
+
 
